108f134e3c9b0ac73d1cb28a45a986136ded68f2
Danilo Silva <danilo170@hotmail.com>
2025-01-01 19:15:33 -0300

    add game state control

--- src/main.lua
+++ src/main.lua
@@ -5,7 +5,8 @@ function love.load()
     target.radius = 50
 
     score = 0
-    timer = 10
+    timer = 0
+    gameState = 1
 
     gameFont = love.graphics.newFont(40)
     sprites = {}
@@ -23,28 +24,37 @@ end
 function love.draw()
     love.graphics.draw(sprites.sky, 0, 0)
 
-    love.graphics.setColor(1, 0, 0)
-    love.graphics.circle("fill", target.x, target.y, target.radius)
 
     love.graphics.setColor(1, 1, 1)
     love.graphics.setFont(gameFont)
-    love.graphics.print(score, 0, 0)
-    love.graphics.print(math.ceil(timer), 300, 0)
+    love.graphics.print("Score: " .. score, 5, 5)
+    love.graphics.print("Time: " .. math.ceil(timer), 300, 5)
 
-    love.graphics.draw(sprites.target, target.x - target.radius, target.y - target.radius)
+    if gameState == 1 then
+        love.graphics.printf("Click anywhere to begin!", 0, 250, love.graphics.getWidth(), "center")
+    end
+
+    if gameState == 2 then
+        love.graphics.draw(sprites.target, target.x - target.radius, target.y - target.radius)
+    end
+
     love.graphics.draw(sprites.crosshairs, love.mouse.getX() - 20, love.mouse.getY() - 20)
 
    
 end
 
 function love.mousepressed(x, y, button, istouch, presses)
-    if button == 1 then
+    if button == 1 and gameState == 2 then
         local mousetoTarget = distanceBetween(x, y, target.x, target.y)
         if mousetoTarget < target.radius then
             increaseScore()
             target.x = math.random(target.radius, love.graphics.getWidth() - target.radius)
             target.y = math.random(target.radius, love.graphics.getHeight() - target.radius)
         end
+    elseif button == 1 and gameState == 1 then
+        gameState = 2
+        timer = 10
+        score = 0
     end
 end
 
@@ -62,5 +72,6 @@ function calculateTimer(dt)
     end
     if timer < 0 then
         timer = 0
+        gameState = 1
     end
 end
